93859c416b72aee3ecd24aa5f206597d7194e603
Deviance <ianrafaelabenido@gmail.com>
2022-07-28 16:26:31 +0800

    Added shading and button toggles

--- Etch.js
+++ Etch.js
@@ -7,8 +7,10 @@ const rainbowButton = document.querySelector('.rainbow');
 const clearButton = document.querySelector('.clear');
 const canvas = document.querySelectorAll('.grids');
 const penColor = document.getElementById('pen');
+const shadingButton = document.querySelector('.shading')
 const sliderValue = document.querySelector('.sliderValue');
-
+const gridButton = document.querySelector('.grid');
+let shadeState = false;
 function grids(columns, rows) {
     container.style.setProperty('--grid-cols', columns);
     container.style.setProperty('--grid-rows', rows);
@@ -16,14 +18,27 @@ function grids(columns, rows) {
     for(i=0; i<(columns*rows); i++){
         let div = document.createElement("div");
         div.classList.add('grids');
-        div.classList.add('gridBorder')
+        div.classList.add('gridBorder');
+        div.classList.add('colored');
         container.appendChild(div);
+    }
+
+    const list = document.querySelectorAll('.colored');
+    for(i=0; i<list.length; i++){
+        list[i].classList.toggle('colored');
     }
 }
 
-document.addEventListener('DOMContentLoaded', grids(16,16));
+document.addEventListener('DOMContentLoaded', () => {
+    randomButton.classList.toggle('randomToggle');
+    eraserButton.classList.toggle('eraserToggle');
+    rainbowButton.classList.toggle('rainbowToggle');
+    shadingButton.classList.toggle('shadingToggle');
+    gridButton.classList.toggle('gridToggle');
+    grids(16,16)
+});
 
-let clearValue = 0; 
+let clearValue = 16; 
 slider.oninput = function() {
 clearValue = this.value
 sliderValue.innerText = `${this.value}x${this.value}`;
@@ -44,26 +59,45 @@ gridToggle.addEventListener("click", ()=> {
         for(i=0; i<list.length; i++){
             list[i].classList.toggle('gridBorder');
         }
+         gridButton.classList.toggle('gridToggle');
     } else {
         gridState = false;
         const list = document.querySelectorAll('.grids');
         for(i=0; i<list.length; i++){
             list[i].classList.toggle('gridBorder');
         }
+        gridButton.classList.toggle('gridToggle');
     }
 })
 
+function shade(color) {
+        let RGB = color.replace(/[^\d,]/g, '').split(',');
+        let r = RGB[0]-(RGB[0]*0.10);
+        let g = RGB[1]-(RGB[1]*0.10);
+        let b = RGB[2]-(RGB[2]*0.10);
+        let shadedRGB = `rgb(${r},${g},${b})`;
+        return shadedRGB;
+}
+
 container.addEventListener("mousedown", (event)=> {
-    if(rainbow==true && eraser==false && random == false && pen == false){
+    if(event.target.classList.contains('colored') && shadeState == true){
+        let style = event.target.style.getPropertyValue("background-color");
+        event.target.style.backgroundColor = shade(style);
+    }
+    else if(rainbow==true && eraser==false && random == false && pen == false){
+       if(event.target.classList.contains('colored')) return;
         event.target.style.backgroundColor = `rgb(${randomColor()})`;
+        event.target.classList.toggle('colored');
     }
     else if(eraser==true && rainbow == false && random == false && pen == false){
         event.target.style.backgroundColor = "white";
     } 
     else if(random == true && eraser == false && rainbow == false && pen == false){
         event.target.style.backgroundColor = etchColor;
+        event.target.classList.toggle('colored');
     } else if(pen == true && eraser == false && rainbow == false && random == false) {
         event.target.style.backgroundColor = etchColor;
+        event.target.classList.toggle('colored');
     } else {
         return;
     }
@@ -71,17 +105,26 @@ container.addEventListener("mousedown", (event)=> {
 
 container.addEventListener("mouseover", (event)=> {
     if(event.buttons != 1) return; 
-        if(rainbow==true && eraser==false && random == false && pen == false){
+        if(event.target.classList.contains('colored') && shadeState == true){
+            let style = event.target.style.getPropertyValue("background-color");
+            event.target.style.backgroundColor = shade(style);
+        }
+        
+        else if(rainbow==true && eraser==false && random == false && pen == false){
+            if(event.target.classList.contains('colored')) return;
             event.target.style.backgroundColor = `rgb(${randomColor()})`;
+            event.target.classList.toggle('colored');
         }
         else if(eraser==true && rainbow == false && random == false && pen == false){
             event.target.style.backgroundColor = "white";
         } 
         else if(random == true && eraser == false && rainbow == false && pen == false){
             event.target.style.backgroundColor = etchColor;
+            event.target.classList.toggle('colored');
         } 
         else if(pen == true && eraser == false && rainbow == false && random == false) {
             event.target.style.backgroundColor = etchColor;
+            event.target.classList.toggle('colored');
         } 
         else {
             return;
@@ -116,15 +159,32 @@ eraserButton.addEventListener("click", ()=> {
     random = false;
     rainbow = false;
     pen = false; 
+    etchColor = "white";
+    eraserButton.classList.toggle('eraserToggle');
 })
 
 rainbowButton.addEventListener("click", ()=> {
+    if(rainbow == true){
+        rainbow=false;
+        pen = true;
+        rainbowButton.classList.toggle('rainbowToggle');
+    } else {
     rainbow = true;
     eraser = false;
     random = false;
     pen = false;
+    rainbowButton.classList.toggle('rainbowToggle');
+    }
 })
 
+shadingButton.addEventListener("click", () => {
+    if(shadeState == true){
+        shadeState = false;
+    } else {
+        shadeState = true;
+    }
+    shadingButton.classList.toggle('shadingToggle');
+})
 
 
 penColor.addEventListener("input", (e)=>{
@@ -135,9 +195,6 @@ penColor.addEventListener("input", (e)=>{
     random = false;
 })
 
-eraserButton.addEventListener("click", ()=> {
-    etchColor = "white";
-});
 
 clearButton.addEventListener("click", ()=> {
     while(container.lastElementChild){
